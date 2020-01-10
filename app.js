78f5821cb87867add67279a1e0c4a6bdabba0b19
Samreaksa <rsam@adalte.com>
2020-01-10 13:29:34 +0700

    Changed :name to v-bind:name

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-Vue.component('app-counter',{
-    template: '#x-template-sample',
-    data(){
-        return{
-            count:0
+Vue.component('app-dev', {
+    template: '#devs-template',
+    props: {
+        name: {
+            type: String,
+            //required: true
         }
     }
+});
+Vue.component('blog-post', {
+    // camelCase in JavaScript
+    props: ['my_property_name'],
+    template: '<h3>{{ my_property_name }}</h3>'
+});
 
-})
 new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        devs: ['Vue', 'WordPress', 'Flutter']
+    }
 })
